2c494732a9fbce5f22f5c5592a026ff7c12eeaa4
Thais Soares Ruiz <thaaaisxd@gmail.com>
2021-09-09 14:10:22 -0600

    Made immigrations to banck and created superuser/ modificade urls.py and home_page.html

--- src/e_commerce/urls.py
+++ src/e_commerce/urls.py
@@ -13,12 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-from .views import home_page, about_page, contact_page
+
+from .views import home_page, about_page, contact_page, login_page, register_page
+
 urlpatterns = [
-        path('', home_page),
-        path('about/', about_page),
-	    path('contact/', contact_page),
-        path('admin/', admin.site.urls),
+	path('', home_page),
+	path('about/', about_page),
+	path('contact/', contact_page),
+    path('login/', login_page),
+    path('register/', register_page),
+	path('admin/', admin.site.urls),
 ]
